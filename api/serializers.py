81a6d30625eade7d510598785e19f2d249b670f0
Maksim Sokolskiy <misokolsky@gmail.com>
2016-12-23 00:21:13 +0200

    feat(api): add `presentee_name` field to `GiftSerializer`

--- api/serializers.py
+++ api/serializers.py
@@ -7,6 +7,11 @@ class GiftSerializer(serializers.ModelSerializer):
     """
     Gift model serializer.
     """
+    presentee_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Gift
         fields = '__all__'
+
+    def get_presentee_name(self, obj):
+        return obj.presentee.get_full_name() or obj.presentee.username
